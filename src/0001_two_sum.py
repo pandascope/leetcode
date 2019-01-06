6e61585da9f03683ccec4f754015220541e26bf2
Panda <pandascope4x@gmail.com>
2019-01-05 23:48:34 -0800

    added comments to two sum

--- src/0001_two_sum.py
+++ src/0001_two_sum.py
@@ -1,3 +1,13 @@
+"""
+Question:
+
+Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+Solution:
+Store target-num as key, and index as value. Return current index and value if num==key
+"""
+
 
 class Solution:
     def twoSum(self, nums, target):
@@ -12,4 +22,6 @@ class Solution:
 
 if __name__ == "__main__":
     res1 = Solution().twoSum([2, 7, 11, 15], 9)
+    res2 = Solution().twoSum([1, 3, 4, 5, 6, 7, 2], 11)
     assert(sorted(res1) == [0, 1])
+    assert (sorted(res2) == [3, 4])
